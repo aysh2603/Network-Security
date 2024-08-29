faded1085fe59e78afd61c358c41e2a9bc76e24e
Aayush Garg <aysh.garg2603@gmail.com>
2024-08-30 02:32:25 +0530

    data validation and transformation pipeline added

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,11 +7,18 @@ class DataIngestionArtifact:
 
 @dataclass
 class DataValidationArtifact:
-    pass
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
 
 @dataclass
 class DataTransformationArtifact:
-    pass
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
 
 @dataclass
 class ModelTrainerArtifact:
